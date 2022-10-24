a73a91c466fa54f91fc909a6829aaf6e5e60cc46
DaLoong <CDaLoong@gmail.com>
2022-10-24 11:22:34 +0800

    feat: 无话

---
+++ Node/test1.js
@@ -0,0 +1,8 @@
+// 获取当前进程的工作目录
+console.log(process.cwd());
+// 获取当前命令中的参数
+console.log(process.argv);
+// 获取操作系统
+console.log(process.platform);
+// 获取全局变量
+console.log(process.env);
---
+++ TypeScript/index.js
@@ -0,0 +1,3 @@
+"use strict";
+var a = '男';
+console.log(typeof a);
